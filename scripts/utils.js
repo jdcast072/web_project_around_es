af6493be84151a53bda67a58e65799fa414cef44
jdcast <jdcast072@gmail.com>
2026-05-16 02:57:53 -0500

    feat: create & implement utils.js handlers & events component

---
+++ scripts/utils.js
@@ -0,0 +1,33 @@
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_is-opened");
+
+    if (openedPopup) {
+      closeModal(openedPopup);
+    }
+  }
+}
+
+function closePopupByOverlay(evt) {
+  if (evt.target.classList.contains("popup")) {
+    closeModal(evt.target);
+  }
+}
+
+function openModal(popup) {
+  popup.classList.add("popup_is-opened");
+
+  document.addEventListener("keydown", handleEscClose);
+
+  popup.addEventListener("mousedown", closePopupByOverlay);
+}
+
+function closeModal(popup) {
+  popup.classList.remove("popup_is-opened");
+
+  document.removeEventListener("keydown", handleEscClose);
+
+  popup.removeEventListener("mousedown", closePopupByOverlay);
+}
+
+export { openModal, closeModal };
